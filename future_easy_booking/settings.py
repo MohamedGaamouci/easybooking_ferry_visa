00ef41dca9f7cf20f92454f38e2d1a1030a8e0d7
MohamedGaamouci <gaamoucimohamed@gmail.com>
2026-01-18 20:11:26 +0100

    secret key

--- future_easy_booking/settings.py
+++ future_easy_booking/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9vy^ovo=nx4q1@ua7b6=5y4(7hd-x)=x698jm)l7=w!d2t!lq0"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
